55879a5b8b182586528ef3853e1f64358c07edf6
wilromeo <wilfriedkp.romeo@mail.com>
2020-12-15 11:18:21 +0100

    fixe

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -11,21 +11,7 @@ RSpec.describe 'Comments Management Function', type: :system do
     FactoryBot.create(:topic, user: @user)
   end
 
-  describe 'Comments manage' do
-    context 'When user login' do
-      it 'comment.' do
 
-        visit new_user_session_path
-        fill_in 'user[email]', with: @user.email
-        fill_in 'user[password]', with: @user.password
-        click_button 'Log in'
-        visit root_path
-        click_on 'comment'
-        fill_in 'comment[comment]', with: 'my comment.'
-        click_on 'commit'
-        expect(page).to have_content 'my comment.'
-      end
-    end
     context 'When user no login ' do
       it 'can not comment.' do
         visit root_path
@@ -35,5 +21,5 @@ RSpec.describe 'Comments Management Function', type: :system do
         expect(page).to have_no_content 'my comment.'
       end
     end
-  end
+  
 end
