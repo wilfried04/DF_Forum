392480d26cf94b99937795c744eef096cb633042
wilromeo <wilfriedkp.romeo@mail.com>
2020-12-17 17:51:56 +0100

    fixe models spec

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-#bundle exec rspec spec/models/task_spec.rb
+#bundle exec rspec spec/models/topic_spec.rb
 RSpec.describe 'Topic model function', type: :model do
   describe 'Validation test' do
     context 'If the topic title is empty' do
@@ -8,17 +8,11 @@ RSpec.describe 'Topic model function', type: :model do
         expect(@topic).not_to be_valid
       end
     end
-    context "if the topic details are empty" do
+    context "if the topic content is empty" do
       it 'validation is caught' do
         @topic =Topic.new(title:'tast')
         expect(@topic).not_to be_valid
       end
-    end
-  end
-  context "if the topic details are empty" do
-    it 'validation is caught' do
-      @topic =Topic.new(title:'tast', content:'content')
-      #expect(topic).to be_valid
     end
   end
 end
